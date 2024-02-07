04283781b57bacc61cb94f69d821be9d56514356
Ahmad Waseem <ahmadwaseem.astechware@gmail.com>
2024-02-07 19:11:45 +0500

    forecast added

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 const API_KEY = "045ac6e30c6e4b83bba92117240602";
+let currentCity = "";
+
+async function apiCall(cityName, method, days, airQuality, alerts) {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.onload = function () {
+      if (request.status === 200) {
+        const response = JSON.parse(this.responseText);
+        resolve(response);
+      } else {
+        reject(`Error: ${request.statusText} or City NOT FOUND!!!!`);
+      }
+    };
+
+    request.onerror = function () {
+      reject("Network error occurred. Please try again.");
+    };
+
+    request.open(
+      "GET",
+      //   `https://api.weatherapi.com/v1/${method}.json?key=${API_KEY}&q=${cityName}&aqi=no`
+
+      `http://api.weatherapi.com/v1/${method}.json?key=${API_KEY}&q=${cityName}&days=${days}&aqi=${airQuality}&alerts=${alerts}`
+    );
+
+    request.send();
+  });
+}
 
 $("#cityName").on("keypress", function () {
   if (event.key === "Enter") {
@@ -8,79 +36,102 @@ $("#cityName").on("keypress", function () {
   }
 });
 
-$("#cityName").on("input", function () {
-  const cityName = $(this).val();
+$("#cityName").on("input", async function () {
+  const city = $(this).val();
 
-  if (cityName.trim() === "") {
+  if (city.trim() === "") {
     clearData();
     return;
   }
 
-  const suggestionRequest = new XMLHttpRequest();
-
-  suggestionRequest.onload = function () {
-    if (suggestionRequest.status === 200) {
-      const response = JSON.parse(this.responseText);
-      console.log(response);
-      displaySuggestions(response);
-    } else {
-      console.error(`Error: ${suggestionRequest.statusText}`);
-    }
-  };
-
-  suggestionRequest.onerror = function () {
-    console.error("Network error occurred while fetching suggestions.");
-  };
-
-  suggestionRequest.open(
-    "GET",
-    `https://api.weatherapi.com/v1/search.json?key=${API_KEY}&q=${cityName}`
-  );
-
-  suggestionRequest.send();
+  try {
+    const response = await apiCall(city, "search");
+    displaySuggestions(response);
+  } catch (error) {
+    alert("ERROR", error);
+  }
 });
 
-function searchWeather() {
-  const cityName = $("#cityName").val();
+async function searchWeather() {
+  currentCity = $("#cityName").val();
 
-  if (cityName.trim() === "") {
+  if (currentCity.trim() === "") {
     alert("City Name cannot be empty");
     return;
-  } else if (!cityName.trim().match(/^[A-Za-z]+$/)) {
+  } else if (!currentCity.trim().match(/^[A-Za-z]+$/)) {
     alert("Enter a valid name with only letters");
     return;
   }
 
-  const request = new XMLHttpRequest();
-  request.onload = function () {
-    if (request.status === 200) {
-      const response = JSON.parse(this.responseText);
-      console.log(response);
-      displayWeatherData(response);
-    } else {
-      alert(`Error: ${request.statusText} or City NOT FOUND!!!!`);
-    }
-  };
-
-  request.onerror = function () {
-    alert("Network error occurred. Please try again.");
-  };
-
-  request.open(
-    "GET",
-    `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${cityName}&aqi=no`
-  );
-
-  request.send();
+  try {
+    const response = await apiCall(currentCity, "current", "", "", "");
+    displayWeatherData(response);
+  } catch (error) {
+    alert("ERROR", error);
+  }
   clearData();
-
   $("#cityName").val("");
+}
+
+async function weatherForecast() {
+  if (currentCity.trim() === "") {
+    alert("Please search for a city first");
+    return;
+  }
+
+  try {
+    const response = await apiCall(currentCity, "forecast", 3, "yes", "yes");
+    //   console.log(response);
+    displayForecastData(response);
+  } catch (error) {
+    alert(error);
+  }
+  clearData();
+  $("#cityName").val("");
+}
+
+function displayForecastData(data) {
+  const { forecast } = data;
+  const forecastOutput = $("#forecastData");
+  forecastOutput.empty();
+
+  forecast.forecastday.forEach((day) => {
+
+    console.log(day.date)
+    console.log(day.day.condition.text)
+
+
+    forecastOutput.append(`
+
+    <div class="col-md-8 col-lg-6 col-xl-4 ">
+
+    <div class="card" 
+    style="border-radius: 35px;">
+    <div class="card-body p-4">
+
+        <div class="d-flex">
+        <h6>${day.date}</h6>
+        </div>
+
+        <div class="d-flex flex-column text-center mb-4">
+            <div><img src=${day.day.condition.icon}></div>
+        <h6 class="display-4 mb-0 font-weight-bold 
+       
+        "> ${day.day.maxtemp_c}°C </h6>
+        <span class="small">${
+        day.day.condition.text
+        }</span>
+        </div>
+    </div>
+    </div>
+    </div>  
+`);
+  });
 }
 
 function displayWeatherData(data) {
   const { location, current, condition } = data;
   const time = location.localtime.split(" ")[1];
-  const date = location.localtime.split(" ")[0];
 
   const output = $("#data");
   output.html(`
